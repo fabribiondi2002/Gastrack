01280a5995420a6d2b9976180fcae49cd7f7e180
fabribiondi2002 <lbiondi733@alumnos.iua.edu.ar>
2025-10-14 17:48:11 -0300

    Primer commit

---
+++ src/main/java/ar/edu/iua/iw3/gastrack/GastrackApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.iua.iw3.gastrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/iua/iw3/gastrack/GastrackApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.iua.iw3.gastrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
