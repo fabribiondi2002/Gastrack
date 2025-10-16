634e0f0134faf7df8800d19facb1466333b58fdf
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-10-16 17:12:41 -0300

    add: product bussines & product repository

---
+++ src/main/java/ar/edu/iua/iw3/gastrack/model/persistence/ProductoRepository.java
@@ -0,0 +1,19 @@
+package ar.edu.iua.iw3.gastrack.model.persistence;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.iua.iw3.gastrack.model.Producto;
+
+@Repository
+public interface ProductoRepository  extends JpaRepository<Producto,Long>
+{
+    /**
+     * Busca un producto por su nombre
+     * @param nombre
+     * @return El producto encontrado o null si no existe
+     */
+    Optional<Producto> findByNombre(String nombre);
+}
