1b89a1cb3565488ebc8311b0a7a25dfc420d8308
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-10-28 18:20:32 -0300

    some fix

--- src/main/java/ar/edu/iua/iw3/gastrack/model/deserializers/TaraJsonDeserializer.java
+++ src/main/java/ar/edu/iua/iw3/gastrack/model/deserializers/TaraJsonDeserializer.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 import ar.edu.iua.iw3.gastrack.model.deserializers.DTO.TaraDTO;
-import ar.edu.iua.iw3.gastrack.util.JsonUtiles;
+import ar.edu.iua.iw3.gastrack.util.JsonUtils;
 
 public class TaraJsonDeserializer extends StdDeserializer<TaraDTO> {
 
@@ -21,8 +21,8 @@ public class TaraJsonDeserializer extends StdDeserializer<TaraDTO> {
 		TaraDTO r = new TaraDTO();
 		JsonNode node = jp.getCodec().readTree(jp);
 
-        long numeroOrden = JsonUtiles.getLong(node, "orden_numero,ordenNumero,order_number,orderNumber,numeroOrden".split(","), -1);
-        double tara = JsonUtiles.getDouble(node, "tara,pesoInicial".split(","), 0.0);
+        long numeroOrden = JsonUtils.getLong(node, "orden_numero,ordenNumero,order_number,orderNumber,numeroOrden".split(","), null);
+        double tara = JsonUtils.getDouble(node, "tara,pesoInicial".split(","), 0.0);
         r.setNumeroOrden(numeroOrden);
         r.setPesoInicial(tara);
 
