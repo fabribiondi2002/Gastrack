e0508fd5bb2fca959501f6c955d97cc45fa6eecb
fabribiondi2002 <lbiondi733@alumnos.iua.edu.ar>
2025-12-16 19:18:31 -0300

    fix: Update CORS configuration to use allowedOriginPatterns instead of allowedOrigins

--- src/main/java/ar/edu/iua/iw3/gastrack/security/SecurityConfiguration.java
+++ src/main/java/ar/edu/iua/iw3/gastrack/security/SecurityConfiguration.java
@@ -96,7 +96,8 @@ public class SecurityConfiguration {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("*"));
+		//config.setAllowedOrigins(List.of("*"));
+		config.setAllowedOriginPatterns(List.of("*"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*")); 
 		config.setAllowCredentials(true);
