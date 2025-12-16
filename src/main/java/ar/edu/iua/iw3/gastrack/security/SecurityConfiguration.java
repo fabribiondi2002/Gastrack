f0bca8da64a1e3d821e42c675b1aea6bccc9efbb
fabribiondi2002 <lbiondi733@alumnos.iua.edu.ar>
2025-12-16 18:32:17 -0300

    HOTFIX: Update CORS configuration to allow all origins

--- src/main/java/ar/edu/iua/iw3/gastrack/security/SecurityConfiguration.java
+++ src/main/java/ar/edu/iua/iw3/gastrack/security/SecurityConfiguration.java
@@ -96,7 +96,7 @@ public class SecurityConfiguration {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("http://localhost:3000"));
+		config.setAllowedOrigins(List.of("*"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*")); 
 		config.setAllowCredentials(true);
